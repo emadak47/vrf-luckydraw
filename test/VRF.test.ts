f02b7803e911242cd6383b77400805895c3be816
tonyconv <tony@conv.finance>
2021-08-26 13:32:45 +0800

    Get rid of redundant bullet points

--- test/VRF.test.ts
+++ test/VRF.test.ts
@@ -44,27 +44,27 @@ describe('VRF contract', function() {
     });
 
     // COMPLETE
-    describe('1. Check const values of the Network kovan', () => {
-        it('1.1 Should return a correct value for fee', () => {
+    describe('Check const values of the Network kovan', () => {
+        it('Should return a correct value for fee', () => {
             expect(_fee).to.equal(toWei('0.1'));
         });
 
-        it('1.2 Should return a correct value for keyHash', () => {
+        it('Should return a correct value for keyHash', () => {
             expect(_keyHash).to.equal('0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4');
         });      
         
-        it('1.3 Should return a correct value for vrfCoordinator', () => {
+        it('Should return a correct value for vrfCoordinator', () => {
             expect(_vrfCoordinator).to.equal('0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9');
         });
 
-        it('1.4 Should return a correct value for LINK address', () => {
+        it('Should return a correct value for LINK address', () => {
             expect(_link).to.equal('0xa36085F69e2889c224210F603D836748e7dC0088');
         });
     });
 
     // COMPLETE
-    describe('2. setCandidatesInfo', () => {
-        it('2.1 Should only allow the owner to modify it', async () => {
+    describe('setCandidatesInfo', () => {
+        it('Should only allow the owner to modify it', async () => {
             await expect(vrfContract.connect(users.stranger)
             .setCandidatesInfo(_candidates)).to.be.revertedWith("Ownable: caller is not the owner");
         });
@@ -72,8 +72,8 @@ describe('VRF contract', function() {
     });
 
     // COMPLETE
-    describe('3. generateWinners', () => {
-        it('3.1 Should select the required No. of winners', async () => {
+    describe('generateWinners', () => {
+        it('Should select the required No. of winners', async () => {
             await vrfContract.setCandidatesInfo(_candidates);
             expect(await vrfContract.getCandidatesNumber()).to.equal(_candidates.length);
             await vrfContract.setWinnersNumber(10);
@@ -88,8 +88,8 @@ describe('VRF contract', function() {
     })
 
     // COMPLETE
-    describe('4. getCandidateInfo', () => {
-        it('4.1 Should check if array and map are set correctly', async () => {
+    describe('getCandidateInfo', () => {
+        it('Should check if array and map are set correctly', async () => {
             await vrfContract.setCandidatesInfo(_candidates);
             expect(await vrfContract.getCandidatesNumber()).to.equal(_candidates.length);
 
@@ -102,15 +102,15 @@ describe('VRF contract', function() {
     });
     
     // COMPLETE
-    describe('5. getLINKAddress()', () => {
-        it('5.1 Should return the address of LINK token', async () => {
+    describe('getLINKAddress()', () => {
+        it('Should return the address of LINK token', async () => {
             expect(await vrfContract.getLINKAddress()).to.equal(_link);
         });
     });
 
     // COMPLETE 
-    describe('6. getRequestFee()', () => {
-        it('6.1 Should return the corresponding fee for the request', async () => {
+    describe('getRequestFee()', () => {
+        it('Should return the corresponding fee for the request', async () => {
             expect(await vrfContract.getRequestFee()).to.equal(_fee);
         })
     });
